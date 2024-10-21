b37eef03d2873cc8a37ab63fb7ac43c3885d4f31
BDraCoder <vuonglh01@gmail.com>
2024-10-21 23:34:32 +0700

    Home-Hero

---
+++ src/components/AppButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppButton = ()=>{
+    return (
+        <header>
+        
+  </header>
+    );
+}
+
+export default AppButton;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 
-const Home = ()=>{
+import '../css/home.css';
+import heroimg from '../images/restauranfood.jpg'
+
+const Home = () => {
     return (<>
-    <section className="hero">
-        <div className="text">
-            <h1></h1>
-            <h3></h3>
-            <p></p>
-        </div>
-        <img src="" alt="" />
-    </section>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-        <h1>Home</h1>
+        <main>
+            <section className="hero">
+                <div className="warper">
+                    <div className="leftContent">
+                        <div className="text">
+                            <h1>Little lemon</h1>
+                            <h3>Chicago</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        {/* Button comp */}
+                    </div>
+                    <img src={heroimg} alt="Restaurant service" />
+                </div>
+            </section>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+        </main>
     </>);
 }
 export default Home;
